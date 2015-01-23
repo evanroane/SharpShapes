3796c9f62cfde05762c3cc94927e7d242d65ab48
evanroane <evan.roane@gmail.com>
2015-01-23 11:22:02 -0600

    added documentation for shapes class

--- SharpShapes/shape.cs
+++ SharpShapes/shape.cs
@@ -9,18 +9,38 @@ namespace SharpShapes
 {
     public class shape
     {
-        //Width, Height, Perimeter, Area, BorderColor, FillColor, NumberOfSides
+        //Perimeter, Area, BorderColor, FillColor, NumberOfSides
+        /// <summary>
+        /// Specifies the color of the interior of the shape when drawn.
+        /// </summary>
         public Color FillColor { get; set; }
+        
+        /// <summary>
+        /// Specifies the color of the bordr of the shape when drawn.
+        /// </summary>
         public Color BorderColor { get; set; }
+        
+        /// <summary>
+        /// Counts the sides of a shape.
+        /// </summary>
         public virtual int SidesCount { get; }
         
-        //returns area of the shape
+        /// <summary>
+        /// Calculates the area of a shape.
+        /// </summary>
+        /// <returns>the area of the shape</returns>
         public abstract decimal Area();
 
-        //returns the perimeter of this shape
+        /// <summary>
+        /// Calculates the perimeter of the shape.
+        /// </summary>
+        /// <returns>the perimeter of this shape</returns>
         public abstract int Perimeter();
 
-        //resizes a shape by percent%
+        /// <summary>
+        /// Scales the size of the shape
+        /// </summary>
+        /// <param name="percent">the percentage by which to scale the shape</param>
         public abstract void scale(int percent)
         { 
             
