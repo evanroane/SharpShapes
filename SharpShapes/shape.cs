ba74a4a1508193accc014cc50c02fa6b69b996e6
evanroane <evan.roane@gmail.com>
2015-01-23 10:11:23 -0600

    Added Color

--- SharpShapes/shape.cs
+++ SharpShapes/shape.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace SharpShapes
 {
     public class shape
     {
         //Width, Height, Perimeter, Area, BorderColor, FillColor, NumberOfSides
+        public Color FillColor { get; set; }
+        public Color BorderColor { get; set; }
+        public virtual int SidesCount { get; }
+        
+        //returns area of the shape
+        public abstract decimal Area();
+
+        //returns the perimeter of this shape
+        public abstract int Perimeter();
+
+        //resizes a shape by percent%
+        public abstract void scale(int percent)
+        { 
+            
+        }
     }
 }
